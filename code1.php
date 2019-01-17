da7fc64e30081c030f19fc5ed298fefef0fef0c1
HugoScott <hugo@hugoscott.com>
2019-01-17 15:47:55 +0100

    ajouté filter dans code1.php

--- code1.php
+++ code1.php
@@ -6,4 +6,12 @@ function noindex_pagination_blog($string= "") {
 	}
     return $string;
 }
+
+add_filter('wpseo_robots', 'noindex_pagination_blog', 999);
+function noindex_pagination_blog($string= "") {
+	if(preg_match("/\/page\/[0-9]+\//i", $_SERVER['REQUEST_URI'])){
+        $string= "noindex,follow";
+	}
+    return $string;
+}
 ?>
