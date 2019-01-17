2c55655349815dfb9dcf45df89694feb7ac33cb7
HugoScott <hugo@hugoscott.com>
2019-01-17 15:49:09 +0100

    modifié nom 2eme filter dans code1.php

--- code1.php
+++ code1.php
@@ -7,8 +7,8 @@ function noindex_pagination_blog($string= "") {
     return $string;
 }
 
-add_filter('wpseo_robots', 'noindex_pagination_blog', 999);
-function noindex_pagination_blog($string= "") {
+add_filter('wpseo_robots', 'noindex_pagination_annuaire', 999);
+function noindex_pagination_annuaire($string= "") {
 	if(preg_match("/\/page\/[0-9]+\//i", $_SERVER['REQUEST_URI'])){
         $string= "noindex,follow";
 	}
